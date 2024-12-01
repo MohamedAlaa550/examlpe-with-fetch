0f55a0d035f506c21a3f5c8b1ba61c0bd83e94cd
MohamedAlaa550 <mohamedalaa.2675@gmail.com>
2024-12-01 03:32:11 +0200

    making modal for each recipe

--- js/index.js
+++ js/index.js
@@ -32,12 +32,22 @@ function displayData(){
                     <img src="${allRecpies[i].image_url}" alt="" class="w-100">
                     <div class="card-body">
                         <h3>${allRecpies[i].title.split(" ", 2).join(" ")}</h3>
-                        <button class="btn btn-danger">Get Info</button>
+                        <button data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="shwoInfo(${allRecpies[i].recipe_id})" class="btn btn-danger">Get Info</button>
                     </div>
                 </div>
             </div>`
     }
     document.getElementById("rowData").innerHTML = cartona
+}
+
+
+async function shwoInfo(id){
+    var response = await(await fetch(`https://forkify-api.herokuapp.com/api/get?rId=${id}`)).json()
+    var recipeInfo = response.recipe
+    document.querySelector(".modal-body").innerHTML = `
+     <img src="${recipeInfo.image_url}" alt="" class="w-100">
+          <h3 class="my-3">${recipeInfo.title.split(" ", 2).join(" ")}</h3>`
+
 }
 
 document.getElementById("searchBtn").addEventListener("click", function () {
